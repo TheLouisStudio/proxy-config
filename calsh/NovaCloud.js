5ef2689b25d5a73815f5d4da1041a8103339c6de
Louis <18650817+TheLouisStudio@users.noreply.github.com>
2026-09-13 22:11:25 +0800

    Change behavior from 'classical' to 'domain'

--- calsh/NovaCloud.js
+++ calsh/NovaCloud.js
@@ -65,7 +65,7 @@ const ruleProviders = {
   },
   "novacloudreject": {
     ...ruleProviderCommon,
-    "behavior": "classical",
+    "behavior": "domain",
     "url": "https://raw.githubusercontent.com/TheLouisStudio/proxy-config/refs/heads/main/calsh/novacloudreject.txt",
     "path": "./ruleset/custom/novacloudreject.yaml"
   },
@@ -101,7 +101,7 @@ const ruleProviders = {
   },
   "novaclouddirect": {
     ...ruleProviderCommon,
-    "behavior": "classical",
+    "behavior": "domain",
     "url": "https://raw.githubusercontent.com/TheLouisStudio/proxy-config/refs/heads/main/calsh/novaclouddirect.txt",
     "path": "./ruleset/custom/novaclouddirect.yaml"
   },
