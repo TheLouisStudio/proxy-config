d3aa74db625fc2bbf6a19fd9fc296b62221b5bcc
Louis <18650817+TheLouisStudio@users.noreply.github.com>
2026-09-13 22:24:13 +0800

    Remove novacloud related rules from NovaCloud.js
    Removed multiple 'novacloud' related rules from the configuration.

--- calsh/NovaCloud.js
+++ calsh/NovaCloud.js
@@ -63,12 +63,6 @@ const ruleProviders = {
     "url": "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/reject.txt",
     "path": "./ruleset/upstream/reject.yaml"
   },
-  "novacloudreject": {
-    ...ruleProviderCommon,
-    "behavior": "domain",
-    "url": "https://raw.githubusercontent.com/TheLouisStudio/proxy-config/refs/heads/main/calsh/novacloudreject.txt",
-    "path": "./ruleset/custom/novacloudreject.yaml"
-  },
   "icloud": {
     ...ruleProviderCommon,
     "behavior": "domain",
@@ -87,23 +81,11 @@ const ruleProviders = {
     "url": "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/google.txt",
     "path": "./ruleset/upstream/google.yaml"
   },
-  "novacloud": {
-    ...ruleProviderCommon,
-    "behavior": "classical",
-    "url": "https://raw.githubusercontent.com/TheLouisStudio/proxy-config/refs/heads/main/calsh/novacloud.txt",
-    "path": "./ruleset/custom/novacloud.yaml"
-  },
   "proxy": {
     ...ruleProviderCommon,
     "behavior": "domain",
     "url": "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/proxy.txt",
     "path": "./ruleset/upstream/proxy.yaml"
-  },
-  "novaclouddirect": {
-    ...ruleProviderCommon,
-    "behavior": "domain",
-    "url": "https://raw.githubusercontent.com/TheLouisStudio/proxy-config/refs/heads/main/calsh/novaclouddirect.txt",
-    "path": "./ruleset/custom/novaclouddirect.yaml"
   },
   "direct": {
     ...ruleProviderCommon,
@@ -221,12 +203,9 @@ const rules = [
   "DOMAIN-SUFFIX,douyin.com,全局直连",
   "DOMAIN-SUFFIX,bilibili.com,全局直连",
   // upstream 规则集
-  "RULE-SET,novaclouddirect,全局直连",
   "RULE-SET,applications,全局直连",
   "RULE-SET,private,全局直连",
-  "RULE-SET,novacloudreject,广告过滤",
   "RULE-SET,reject,广告过滤",
-  "RULE-SET,novacloud,节点选择",
   "RULE-SET,Microsoft,微软服务",
   "RULE-SET,icloud,iCloud服务",
   "RULE-SET,apple,苹果服务",
