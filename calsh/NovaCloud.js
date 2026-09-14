6c846538ff1feb6f2fd86e856ebd66557c8d7fc4
Louis <18650817+TheLouisStudio@users.noreply.github.com>
2026-09-14 15:01:38 +0800

    Remove domestic and foreign DNS server entries

--- calsh/NovaCloud.js
+++ calsh/NovaCloud.js
@@ -2,14 +2,12 @@
 // 国内DNS服务器
 const domesticNameservers = [
   "https://223.5.5.5/dns-query", // 阿里DoH
-  "https://223.6.6.6/dns-query", // 阿里DoH
   "https://doh.pub/dns-query" // 腾讯DoH
 ];
 // 国外DNS服务器
 const foreignNameservers = [
   "https://1.1.1.1/dns-query", // CloudflareDNS
   "https://8.8.8.8/dns-query", // GoogleDNS
-  "https://208.67.222.222/dns-query", // OpenDNS
   "https://9.9.9.9/dns-query" // Quad9 恶意域名过滤
 ];
 // DNS配置
