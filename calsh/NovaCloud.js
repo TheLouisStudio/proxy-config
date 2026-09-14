6a0a45a8961288aad22cc0ad1b3b1d52332bf878
Louis <18650817+TheLouisStudio@users.noreply.github.com>
2026-09-14 14:55:39 +0800

    Update domestic DNS server address

--- calsh/NovaCloud.js
+++ calsh/NovaCloud.js
@@ -2,7 +2,7 @@
 // 国内DNS服务器
 const domesticNameservers = [
   "https://223.5.5.5/dns-query", // 阿里DoH
-  "https://dns.alidns.com/dns-query", // 阿里DoH
+  "https://223.6.6.6/dns-query", // 阿里DoH
   "https://doh.pub/dns-query" // 腾讯DoH
 ];
 // 国外DNS服务器
